b31b820f40c159c47a675f19655e94aa2b64fea4
supergonkas <goncalo-matos@hotmail.com>
2013-10-21 18:16:03 +0100

    Adding new cleaning forms

--- Ultra Cleaner/Form1.vb
+++ Ultra Cleaner/Form1.vb
@@ -10,7 +10,5 @@
 
     Private Sub Button2_Click(sender As System.Object, e As System.EventArgs) Handles Button2.Click
         Form2.Show()
-        
-
     End Sub
 End Class
--- Ultra Cleaner/Form2.vb
+++ Ultra Cleaner/Form2.vb
@@ -11,7 +11,8 @@
     Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
         MsgBox("The cleaning will start", MsgBoxStyle.Information, "Info")
         Timer1.Start()
-        My.Computer.FileSystem.DeleteFile("")
+        My.Computer.FileSystem.DeleteDirectory("C:\WINDOWS\Temp",
+FileIO.DeleteDirectoryOption.DeleteAllContents)
         MsgBox("Cleaning Stoped", MsgBoxStyle.Information, "Info")
     End Sub
 
